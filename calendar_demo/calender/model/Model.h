4d917394282a3da89404fca4b1b9ec1a068e4fac
fangxiang <fx.fangxiang@gmail.com>
2013-05-25 15:26:46 +0800

    update event, delete event API

--- calendar_demo/calender/model/Model.h
+++ calendar_demo/calender/model/Model.h
@@ -1,7 +1,7 @@
 
 #import <Foundation/Foundation.h>
 #import "User.h"
-
+#import "Event.h"
 
 @interface Model : NSObject
 
@@ -13,6 +13,18 @@
  Call WebService API to get the lastest events.s
  */
 -(void) getEvents:(void (^)(NSInteger error, NSArray* events))callback;
+
+
+/**
+ Update event title and description
+ */
+-(void) updateEvent:(Event *) event andCallback:(void (^)(NSInteger error))callback;
+
+/**
+ Delete a event
+ */
+-(void) deleteEvent:(Event *) event andCallback:(void (^)(NSInteger error))callback;
+
 
 +(Model *) getInstance;
 
--- calendar_demo/calender/model/Model.m
+++ calendar_demo/calender/model/Model.m
@@ -46,6 +46,64 @@ static Model * instance;
     }];
 }
 
+
+-(void) updateEvent:(Event *) event andCallback:(void (^)(NSInteger error))callback
+{
+    NSString * url = [NSString stringWithFormat:@"%s/api/v1/event/%d", HOST, event.id];
+
+    NSLog(@"url=%@", url);
+
+
+    NSMutableURLRequest *request = [Utils createHttpRequest:url andMethod:@"PUT"];
+
+
+    NSMutableDictionary * dict = [[NSMutableDictionary alloc] init];
+    [dict setObject:event.title forKey:@"title"];
+    [dict setObject:event.description forKey:@"description"];
+
+    NSString * postContent = [Utils dictionary2String:dict];
+    NSData * postData = [postContent dataUsingEncoding:NSUTF8StringEncoding];
+    [request setHTTPBody:postData];
+
+    [[UserModel getInstance] setAuthHeader:request];
+
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * resp, NSData * data, NSError * error) {
+        NSHTTPURLResponse * httpResp = (NSHTTPURLResponse*) resp;
+        int status = httpResp.statusCode;
+
+        if(status == 202) {
+            callback(0);
+        } else {
+            callback(-1);
+        }
+    }];
+
+}
+
+
+-(void) deleteEvent:(Event *) event andCallback:(void (^)(NSInteger error))callback
+{
+    NSString * url = [NSString stringWithFormat:@"%s/api/v1/event/%d", HOST, event.id];
+
+    NSLog(@"url=%@", url);
+
+    NSMutableURLRequest *request = [Utils createHttpRequest:url andMethod:@"DELETE"];
+
+    [[UserModel getInstance] setAuthHeader:request];
+
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * resp, NSData * data, NSError * error) {
+        NSHTTPURLResponse * httpResp = (NSHTTPURLResponse*) resp;
+        int status = httpResp.statusCode;
+
+        if(status == 204) {
+            callback(0);
+        } else {
+            callback(-1);
+        }
+    }];
+}
+
+
 +(Model *) getInstance
 {
     if(instance == nil) {
