b49510bd3d1bea780a705390b11838b5f617e314
fang xiang <fx.fangxiang@gmail.com>
2013-06-03 17:53:42 +0800

    add commit

--- calendar_demo/calender/AppDelegate.h
+++ calendar_demo/calender/AppDelegate.h
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.h
-//  calender
-//
-//  Created by fang xiang on 13-5-4.
-//  Copyright (c) 2013年 fang xiang. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
--- calendar_demo/calender/AppDelegate.m
+++ calendar_demo/calender/AppDelegate.m
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.m
-//  calender
-//
-//  Created by fang xiang on 13-5-4.
-//  Copyright (c) 2013年 fang xiang. All rights reserved.
-//
 
 #import "AppDelegate.h"
 
