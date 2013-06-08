21e4f2fe8cda6bfe35244258eefd098bfbe23bad
fang xiang <fx.fangxiang@gmail.com>
2013-06-08 10:00:47 +0800

    Login View

---
+++ calendar_demo/calender/views/LoginView.m
@@ -0,0 +1,31 @@
+//
+//  LoginView.m
+//  calender
+//
+//  Created by fang xiang on 13-6-8.
+//  Copyright (c) 2013年 fang xiang. All rights reserved.
+//
+
+#import "LoginView.h"
+
+@implementation LoginView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
