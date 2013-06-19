6c78adae8bccc3d9c45a507ec950d36c3977d33c
zyax86 <zyax86@foxmail.com>
2013-06-19 23:33:02 +0800

    fix

--- calendar_demo/calender/Events/AddEventViewController.h
+++ calendar_demo/calender/Events/AddEventViewController.h
@@ -1,7 +1,15 @@
-
+//
+//  AddEventViewController.h
+//  calender
+//
+//  Created by zyax86 on 13-6-19.
+//  Copyright (c) 2013年 fang xiang. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
 
 @interface AddEventViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
 
 @end
--- calendar_demo/calender/Events/AddEventViewController.m
+++ calendar_demo/calender/Events/AddEventViewController.m
@@ -1,22 +1,25 @@
-
+//
+//  AddEventViewController.m
+//  calender
+//
+//  Created by zyax86 on 13-6-19.
+//  Copyright (c) 2013年 fang xiang. All rights reserved.
+//
 
 #import "AddEventViewController.h"
-#import <UIKit/UIImagePickerController.h>
-//#import "AddLocationViewController.h"
 #import "AddEventView.h"
 
 @interface AddEventViewController ()
-//<UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIActionSheetDelegate>
 
 @end
 
 @implementation AddEventViewController
 
-- (id)init
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super init];
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        
+        // Custom initialization
     }
     return self;
 }
@@ -24,27 +27,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    [self.view addSubview:scrollView];
-    [scrollView release];
-    
-//    NSArray* nibView =  [[NSBundle mainBundle] loadNibNamed:@"AddEventView" owner:self options:nil];
-//    AddEventView *addEventView = (AddEventView*)[nibView objectAtIndex:0];
-//    [scrollView addSubview:addEventView];
-    
-    
-//    view.frame = CGRectMake(0, 0, 320, PlanView_HEIGHT);
-//    AddEventView *eventView = [AddEventView alloc] ini
-    
-    
-    
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    [self.navigationController setNavigationBarHidden:YES];
+    // Do any additional setup after loading the view from its nib.
+    NSArray* nibView =  [[NSBundle mainBundle] loadNibNamed:@"AddEventView" owner:self options:nil];
+    AddEventView *addEventView = (AddEventView*)[nibView objectAtIndex:0];
+    [self.scrollView addSubview:addEventView];
+    [self.scrollView setContentSize:addEventView.frame.size];
 }
 
 - (void)didReceiveMemoryWarning
@@ -53,63 +40,19 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark -
-#pragma mark AddEventPhoto
-//- (IBAction)btnAddEventPhoto:(id)sender {
-//    UIActionSheet *menu = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:@"Picker Photo From Album" otherButtonTitles:@"Picker Photo From Camera", nil];
-//    [menu showInView:self.view];
-//}
-
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
-{
-    if (buttonIndex == 0) {
-        [self getImageFrom:UIImagePickerControllerSourceTypePhotoLibrary];
-    }
-    else if(buttonIndex == 1)
-    {
-        [self getImageFrom:UIImagePickerControllerSourceTypeCamera];
-    }
+- (void)viewDidUnload {
+    [self setScrollView:nil];
+    [super viewDidUnload];
 }
 
-- (void)getImageFrom:(UIImagePickerControllerSourceType)type
-{
-    UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
-    ipc.sourceType = type;
-    ipc.delegate = self;
-    [self presentModalViewController:ipc animated:YES];
+- (IBAction)Cancel:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo
-{
-//    self.eventPhotoView.image = image;
-    [picker dismissModalViewControllerAnimated:YES];
+
+- (IBAction)Create:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
-#pragma mark -
-#pragma mark AddLocation
-
-- (IBAction)btnAddLocation:(id)sender {
-//    AddLocationViewController *addLocation = [[AddLocationViewController alloc] initWithNibName:@"AddLocationViewController" bundle:nil];
-//    [self.navigationController pushViewController:addLocation animated:YES];
-}
-
-//- (IBAction)btnInvitePeople:(id)sender {
-//}
-//
-//- (IBAction)btnAddEventTitle:(id)sender {
-//}
-//
-//- (IBAction)btnAddDayOrTime:(id)sender {
-//}
-
-
-//- (IBAction)Cancel:(id)sender {
-//    [self.navigationController popViewControllerAnimated:YES];
-//}
-
-- (void)dealloc
-{
-//    [self setEventPhotoView:nil];
-}
 
 @end
