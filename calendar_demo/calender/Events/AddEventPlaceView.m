913bb31c0cc3c6fa4b10c0629d0f4f55181674ce
fang xiang <fx.fangxiang@gmail.com>
2013-07-11 17:29:55 +0800

    Create Event View

---
+++ calendar_demo/calender/Events/AddEventPlaceView.m
@@ -0,0 +1,37 @@
+//
+//  AddEventPlaceView.m
+//  calender
+//
+//  Created by fang xiang on 13-7-11.
+//  Copyright (c) 2013年 fang xiang. All rights reserved.
+//
+
+#import "AddEventPlaceView.h"
+
+@implementation AddEventPlaceView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+-(void)dealloc
+{
+    self.label = nil;
+    [super dealloc];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
