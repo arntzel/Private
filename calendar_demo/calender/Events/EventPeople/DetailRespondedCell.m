43540447198fa7a60cb2274f163bf55556542b10
zyax86 <zyax86@foxmail.com>
2013-10-07 18:35:52 +0800

    update detail invitees cell

--- calendar_demo/calender/Events/EventPeople/DetailRespondedCell.m
+++ calendar_demo/calender/Events/EventPeople/DetailRespondedCell.m
@@ -30,6 +30,10 @@
 {
     self.peopleHeader.layer.cornerRadius = self.peopleHeader.frame.size.width/2;
     self.peopleHeader.layer.masksToBounds = YES;
+    [self.agreeTimeMark setHidden:YES];
+    [self.agreeTimeLabel setHidden:YES];
+    [self.declindTimeMark setHidden:YES];
+    [self.declindTimeLabel setHidden:YES];
 }
 
 - (void)setHeaderImage:(UIImage *)image
@@ -47,14 +51,71 @@
     self.peopleName.text = name;
 }
 
+- (void)upUserNamePlace
+{
+    CGRect frame = self.peopleName.frame;
+    frame.origin.y = self.peopleHeader.frame.origin.y;
+    self.peopleName.frame = frame;
+}
+
 - (void)setAgreeTime:(NSArray*)array
 {
-    
+    if ([array count] > 0) {
+        [self.agreeTimeMark setHidden:NO];
+        [self.agreeTimeLabel setHidden:NO];
+        [self upUserNamePlace];
+        
+        CGRect frame = self.agreeTimeMark.frame;
+        frame.origin.y = self.peopleName.frame.origin.y + self.peopleName.frame.size.height + 7;
+        self.agreeTimeMark.frame = frame;
+        
+        frame = self.agreeTimeLabel.frame;
+        frame.origin.y = self.peopleName.frame.origin.y + self.peopleName.frame.size.height + 4;
+        self.agreeTimeLabel.frame = frame;
+        
+        
+        
+        frame = self.frame;
+        frame.size.height = self.agreeTimeLabel.frame.origin.y + self.agreeTimeLabel.frame.size.height + 10;
+        self.frame = frame;
+    }
 }
 
 - (void)setDeclindTime:(NSArray*)array
 {
-    
+    if ([array count] > 0) {
+        [self.declindTimeMark setHidden:NO];
+        [self.declindTimeLabel setHidden:NO];
+        if (self.agreeTimeLabel.hidden == YES) {
+            [self upUserNamePlace];
+            
+            CGRect frame = self.declindTimeMark.frame;
+            frame.origin.y = self.peopleName.frame.origin.y + self.peopleName.frame.size.height + 7;
+            self.declindTimeMark.frame = frame;
+            
+            frame = self.declindTimeLabel.frame;
+            frame.origin.y = self.peopleName.frame.origin.y + self.peopleName.frame.size.height + 4;
+            self.declindTimeLabel.frame = frame;
+            
+            frame = self.frame;
+            frame.size.height = self.declindTimeLabel.frame.origin.y + self.declindTimeLabel.frame.size.height + 10;
+            self.frame = frame;
+        }
+        else
+        {
+            CGRect frame = self.declindTimeMark.frame;
+            frame.origin.y = self.agreeTimeLabel.frame.origin.y + self.agreeTimeLabel.frame.size.height + 7;
+            self.declindTimeMark.frame = frame;
+            
+            frame = self.declindTimeLabel.frame;
+            frame.origin.y = self.agreeTimeLabel.frame.origin.y + self.agreeTimeLabel.frame.size.height + 4;
+            self.declindTimeLabel.frame = frame;
+            
+            frame = self.frame;
+            frame.size.height = self.declindTimeLabel.frame.origin.y + self.declindTimeLabel.frame.size.height + 10;
+            self.frame = frame;
+        }
+    }
 }
 
 @end
