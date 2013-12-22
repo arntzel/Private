2f9c233a91ec3d9a8e88844df69c70e5cccee6ba
zyax86 <zyax86@foxmail.com>
2013-12-22 13:07:02 +0800

    fix https://www.dropbox.com/s/rfx90dt7ou4xp60/IMG_4675.MOV

--- calendar_demo/calender/Utils/DeviceInfo.m
+++ calendar_demo/calender/Utils/DeviceInfo.m
@@ -1,6 +1,8 @@
 #import "DeviceInfo.h"
+#import "SysInfo.h"
 
 @implementation DeviceInfo
+
 + (float)fullScreenHeight
 {
     return [[UIScreen mainScreen] bounds].size.height;
@@ -9,10 +11,13 @@
 + (CGRect)fullScreenFrame
 {
     CGFloat statusBarHeight = 0.0f;
-    if (![UIApplication sharedApplication].statusBarHidden)
-    {
-        statusBarHeight = 20.0f;
+    if ([SysInfo version] < 7.0) {
+        if (![UIApplication sharedApplication].statusBarHidden)
+        {
+            statusBarHeight = 20.0f;
+        }
     }
+
     CGRect frame = [[UIScreen mainScreen] bounds];
     frame.size.height -= statusBarHeight;
 
