c0e56b52834c58e1407324e63cc5ae3106e1a4ec
Jeffrey <zhiwehu@gmail.com>
2014-03-23 11:29:40 -0400

    We should not consider the birthday as conflict

--- calendar_demo/calender/coredata/CoreDataModel.m
+++ calendar_demo/calender/coredata/CoreDataModel.m
@@ -463,7 +463,7 @@ static CoreDataModel * instance;
     //NSPredicate * predicate = [NSPredicate predicateWithFormat:@"((confirmed = true) AND ((start >= %@ AND start <= %@) OR (end >= %@ AND end <= %@)) AND (eventType==0))", start,  end, start,  end];
     
     // We should not consider the birthday as conflict
-    NSPredicate * predicate = [NSPredicate predicateWithFormat:@"((confirmed = true) AND ((start >= %@ AND start <= %@) OR (end >= %@ AND end <= %@) OR (start < %@ AND end > %@) AND (eventType!=4)))", start,  end, start,  end, start, end];
+    NSPredicate * predicate = [NSPredicate predicateWithFormat:@"((confirmed = true) AND ((start >= %@ AND start <= %@) OR (end >= %@ AND end <= %@) OR (start < %@ AND end > %@)) AND (eventType!=4))", start,  end, start,  end, start, end];
     [fetchRequest setPredicate:predicate];
     
 //    NSArray * results = [managedObjectContext executeFetchRequest:fetchRequest error:nil];
